7cef6ba60cab0deb7dd639ad1c99b868c2db7078
Entropy <bmsac0001@gmail.com>
2019-09-26 05:36:55 +0800

    Add commentry for Make.sh and update README

--- Make.sh
+++ Make.sh
@@ -1,3 +1,31 @@
+## eemacs-ext Make.sh --- The core make procedure for eemacs-ext 
+
+# * Copyright (C) 2019  Entropy
+# #+BEGIN_EXAMPLE
+# Author:        Entropy <bmsac0001@gmail.com>
+# Maintainer:    Entropy <bmsac001@gmail.com>
+# URL:           https://github.com/c0001/entropy-emacs-extensions/blob/master/Make.sh
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# #+END_EXAMPLE
+# * Commentary:
+#
+# The main procedure of making eemacs-ext project, type 
+# 'bash Make.sh' to see the usage prompts.
+#
+# * Code
+# ** preface
 EemacsextMake_SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$EemacsextMake_SOURCE" ]; do # resolve $EemacsextMake_SOURCE until the file is no longer a symlink
   EemacsextMake_DIR="$( cd -P "$( dirname "$EemacsextMake_SOURCE" )" >/dev/null && pwd )"
@@ -34,9 +62,6 @@ EemacsextMake_dir_nontrail_slash ()
 }
 
 EemacsextMake_DIR="$(EemacsextMake_dir_nontrail_slash ${EemacsextMake_DIR} lame)"
-
-
-# * Code
 
 # ** variable declaration
 EemacsextMake_melpadir=${EemacsextMake_DIR}/elements/submodules/melpa
